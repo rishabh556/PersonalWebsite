2a6cb9e32253beca094dc29847833ef7ad66dbf3
Rishabh Tiwari <r.tiwari6396@gmail.com>
2021-06-16 19:31:07 +0530

    first commit

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -9,11 +9,11 @@ const Modal = ({ closeModal }) => {
       <div onClick={(e)=>e.stopPropagation()} className="modal-container">
 
         <div className="upper-section">
-          <h1>Huge Announcement!!</h1>
+          <h2>Hello,</h2>
           <p className="close"> &times;</p>
         </div>
         <div className="middle-section">
-          <h3>This portfolio i built while learning react!!</h3>
+          <h3>Welcome to my portfolio, I hope you will like it</h3>
         </div>
         <div className="lower-section">
           <a onClick={closeModal} className="modal-btn btn-red">close</a>
